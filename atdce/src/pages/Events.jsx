84f1c6fd4cb3c3f83beba7a30dc6f9bcb2ad2c87
onshermessi <oh@users.noreply.github.com>
2024-05-13 22:33:14 +0100

    woo

--- atdce/src/pages/Events.jsx
+++ atdce/src/pages/Events.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Events.css";
 import EventForm from "../components/EventForm.jsx";
 
 import headerbg from "../assets/headerbg.png";
 import nahjlboustaevent from "../assets/nahjlboustaevent.png";
+import { useActionData } from "react-router-dom";
 
 export default function Events() {
+  const [showText, setShowText] = useState({
+    whereAndWhen: false,
+    planning: false,
+    timeline: false,
+  });
+
+  const [activeSection, setActiveSection] = useState("");
+
+  const handleToggleText = (section) => {
+    setShowText((prevShowText) => ({
+      ...prevShowText,
+      [section]: !prevShowText[section],
+    }));
+
+    setActiveSection((prevActiveSection) => {
+      // If the clicked section is already active, toggle it off
+      if (prevActiveSection === section && showText[section]) {
+        return "";
+      } else {
+        // Otherwise, set the clicked section as active
+        return section;
+      }
+    });
+    if (activeSection && activeSection !== section) {
+      setShowText((prevShowText) => ({
+        ...prevShowText,
+        [activeSection]: false,
+      }));
+    }
+  };
+
+  {
+    /* (prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+    setActiveSection((prevSection) => (prevSection === section ? "" : section)); */
+  }
   return (
     <>
       <header className="header">
@@ -42,6 +81,48 @@ export default function Events() {
             remaining essentially unchanged. It was popularised.
           </p>
           <p style={{ fontWeight: 700 }}>See more</p>
+
+          <div className="titles-with-borders">
+            <h3
+              onClick={() => handleToggleText("whereAndWhen")}
+              className={activeSection === "whereAndWhen" ? "active" : ""}
+            >
+              Where & When
+            </h3>
+            {
+              <p
+                className={`whereAndWhen ${
+                  showText.whereAndWhen ? "show" : ""
+                }`}
+              >
+                Details about Where & When...
+              </p>
+            }
+            <h3
+              onClick={() => handleToggleText("planning")}
+              className={activeSection === "planning" ? "active" : ""}
+            >
+              Planning
+            </h3>
+            {
+              <p className={`planning ${showText.planning ? "show" : ""}`}>
+                {" "}
+                Details about Planning...
+              </p>
+            }
+            <h3
+              onClick={() => handleToggleText("timeline")}
+              className={activeSection === "timeline" ? "active" : ""}
+            >
+              Timeline
+            </h3>
+            {
+              <p className={`timeline ${showText.timeline ? "show" : ""}`}>
+                {" "}
+                Details about Timeline...
+              </p>
+            }
+          </div>
         </div>
       </div>
     </>
