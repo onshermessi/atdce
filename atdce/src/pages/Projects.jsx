df19fa7fd7818f759a398de49a2efd2266bdd565
onshermessi <oh@users.noreply.github.com>
2024-05-24 12:10:36 +0100

    projects

--- atdce/src/pages/Projects.jsx
+++ atdce/src/pages/Projects.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../index.css";
 import "./Projects.css";
+import ProjHeroSection from "../components/ProjHeroSection";
 
 export default function Projects() {
-  return <></>;
+  return (
+    <>
+      <ProjHeroSection />
+    </>
+  );
 }
